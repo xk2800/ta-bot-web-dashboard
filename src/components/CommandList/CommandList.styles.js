00739ffd7b0aa72961f3e2cef0c9ff6b525f66d5
Shaunmak1214 <shaonmak@gmail.com>
2021-07-30 00:12:29 +0800

    feat: styles

--- src/components/CommandList/CommandList.styles.js
+++ src/components/CommandList/CommandList.styles.js
@@ -1,37 +1,42 @@
 import styled from "styled-components";
+import { Colors } from "../../constants";
 
 export const Wrapper = styled.div`
-    /* max-width: 1280px; */
-    padding: 10px 16px;
-    margin: 20px 500px;
-    border: 1px solid #fafafa;
-    text-align: left;
-    
+  /* max-width: 1280px; */
+  padding: 10px 16px;
+  margin: 20px 500px;
+  border: 1px solid #fafafa;
+  text-align: left;
 
-    h3{
-        font-size: 20px;
-        color: var(--redColor);
-        margin-bottom: 8px;
-    }
+  h3 {
+    font-size: 20px;
+    color: ${Colors.Primary};
+    margin-bottom: 8px;
+  }
 
-    .short_text{
-        color: var(--white);
-        font-weight: 100;
-    }
+  .short_text {
+    color: ${Colors.White};
+    font-weight: 100;
+  }
 
+  code {
+    font-size: 20px;
+    color: ${Colors.Primary};
+    margin-bottom: 8px;
+  }
 `;
 
 export const Content = styled.div`
-    color: var(--white);
-    
-    p{
-        margin: 10px 0;
-    }
+  color: var(--white);
 
-    .code{
-        background: var(--blueColor);
-        padding: 5px 0;
-        padding-left: 5px;
-        border-radius: 10px;
-    }
+  p {
+    margin: 10px 0;
+  }
+
+  .code {
+    background: ${Colors.Blue};
+    padding: 5px 0;
+    padding-left: 5px;
+    border-radius: 10px;
+  }
 `;
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+module.exports.Colors = {
+  Primary: "#BC2B29",
+  White: "#FFFFFF",
+  Black: "#000000",
+  Blue: "#0F3856",
+};

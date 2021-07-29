7938569f9daba6fb5d305b8f4bc1983a5eadb5b9
Restyled.io <commits@restyled.io>
2021-07-29 14:52:47 +0000

    Restyled by prettier

--- src/components/CommandList/CommandList.js
+++ src/components/CommandList/CommandList.js
@@ -1,30 +1,37 @@
-import {Content, Wrapper} from "./CommandList.styles";
+import { Content, Wrapper } from "./CommandList.styles";
 
-const CommandList = ({commands, head}) => {
+const CommandList = ({ commands, head }) => {
   let commandsList = commands.commands;
   if (commandsList.length < 0) {
-    return (<>{" "}<p>No Commands</p>{" "}
-      </>);
+    return (
+      <>
+        {" "}
+        <p>No Commands</p>{" "}
+      </>
+    );
   } else {
     return (
-        <div className = "command-list"><h2>{head}<
-            /h2>
+      <div className="command-list">
+        <h2>{head}</h2>
         {commandsList.map((cmd) => (
           <Wrapper>
             <div className="command-listed" key={cmd.id}>
               <h3>
-                <code>{cmd.command}</code>{
-            " "}<span className = "short_text">-
-        {cmd.short_text}</span>
-              </h3><Content>
-        <p>Usage: </p>
-                <p>{cmd.explanation}</p><code>
-        <p className = "code">{cmd.example}</p>
+                <code>{cmd.command}</code>{" "}
+                <span className="short_text">-{cmd.short_text}</span>
+              </h3>
+              <Content>
+                <p>Usage: </p>
+                <p>{cmd.explanation}</p>
+                <code>
+                  <p className="code">{cmd.example}</p>
                 </code>
-        </Content>
-            </div></Wrapper>
+              </Content>
+            </div>
+          </Wrapper>
         ))}
-      </div>);
+      </div>
+    );
   }
 };
 
